10eed5a34f1a91ed74b3583f141cfcd2adb809d8
darthvonnugaj <75%uk9gdkeGIT>
2018-04-17 19:29:04 +0200

    Bottom menu implementation #2

--- app/src/main/java/com/example/wadim/osmdroid_test/MainActivity.java
+++ app/src/main/java/com/example/wadim/osmdroid_test/MainActivity.java
@@ -4,7 +4,20 @@ import android.app.Activity;
 import android.content.Context;
 import android.preference.PreferenceManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.MenuItem;
+
+import com.example.wadim.osmdroid_test.fragment.CartFragment;
+import com.example.wadim.osmdroid_test.fragment.GiftsFragment;
+import com.example.wadim.osmdroid_test.fragment.StoreFragment;
+import com.example.wadim.osmdroid_test.helper.BottomNavigationBehavior;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
@@ -14,17 +27,33 @@ import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.MinimapOverlay;
 import org.osmdroid.views.overlay.gridlines.LatLonGridlineOverlay2;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     MapView map = null;
     MinimapOverlay mMinimapOverlay;
+    private ActionBar toolbar;
 
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // attaching bottom sheet behaviour - hide / show on scroll
+        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) navigation.getLayoutParams();
+        layoutParams.setBehavior(new BottomNavigationBehavior());
+
+        // load the store fragment by default
+        toolbar.setTitle("Map");
+        loadFragment(new GiftsFragment());
 
         //handle permissions first, before map is created. not depicted here
 
         //load/initialize the osmdroid configuration, this can be done
-        Context ctx = getApplicationContext();
+        /*Context ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         //setting this before the layout is inflated is a good idea
         //it 'should' ensure that the map has a writable location for the map cache, even without permissions
@@ -56,10 +85,10 @@ public class MainActivity extends Activity {
         mMinimapOverlay.setZoomDifference(4);
         //optionally, you can set the minimap to a different tile source
         //mMinimapOverlay.setTileSource(....);
-        map.getOverlays().add(this.mMinimapOverlay);
+        map.getOverlays().add(this.mMinimapOverlay);*/
     }
 
-    public void onResume(){
+    /*public void onResume(){
         super.onResume();
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
@@ -75,5 +104,41 @@ public class MainActivity extends Activity {
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+    }*/
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_map:
+                    toolbar.setTitle("Map");
+                    fragment = new GiftsFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_list:
+                    toolbar.setTitle("Route List");
+                    fragment = new StoreFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_folder:
+                    toolbar.setTitle("Saved Routes");
+                    fragment = new CartFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/wadim/osmdroid_test/fragment/DetailsFragment.java
@@ -0,0 +1,38 @@
+package com.example.wadim.osmdroid_test.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wadim.osmdroid_test.R;
+
+
+public class DetailsFragment extends Fragment {
+
+    public DetailsFragment() {
+        // Required empty public constructor
+    }
+
+    public static DetailsFragment newInstance(String param1, String param2) {
+        DetailsFragment fragment = new DetailsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+
+}
